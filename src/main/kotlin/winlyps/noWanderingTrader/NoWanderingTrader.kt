244f3d306362b1f85248dbe58f6217e74162544a
Winlypse <nts3@o2.pl>
2024-10-22 15:16:29 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noWanderingTrader/NoWanderingTrader.kt
@@ -0,0 +1,14 @@
+package winlyps.noWanderingTrader
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoWanderingTrader : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
