ae29aed3b6d7b6c94a3e4dec45177efa1f695f43
Winlypse <nts3@o2.pl>
2024-10-22 15:16:43 +0200

    Changes!

--- src/main/kotlin/winlyps/noWanderingTrader/NoWanderingTrader.kt
+++ src/main/kotlin/winlyps/noWanderingTrader/NoWanderingTrader.kt
@@ -6,9 +6,14 @@ class NoWanderingTrader : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("doTraderSpawning", "false")
+        }
+        logger.info("NoWanderingTrader plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoWanderingTrader plugin has been disabled.")
     }
 }
